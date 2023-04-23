8f77d26c748ed44725827d539cd8f541da08b3ba
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-04-23 18:54:09 +0200

    removeSwipe

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,39 +5,11 @@ import TableInputs from './TableInputs'
 
 export default function Main() {
   const [activeTab, setActiveTab] = useState('dom')
-  const [touchStartX, setTouchStartX] = useState(0)
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab)
-  }
-
-  const handleTouchStart = (event) => {
-    setTouchStartX(event.touches[0].clientX)
-  }
-
-  const handleTouchEnd = (event) => {
-    const touchEndX = event.changedTouches[0].clientX
-    const touchDistance = touchEndX - touchStartX
-
-    if (touchDistance < 0 && activeTab === 'dom') {
-      setActiveTab('table')
-    } else if (touchDistance > 0 && activeTab === 'table') {
-      setActiveTab('dom')
-    }
-  }
 
   return (
     <>
-      <Tabs
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-        onTabClick={handleTabClick}
-      />
-      <div
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        style={{ touchAction: 'pan-y' }}
-      >
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div style={{ touchAction: 'pan-y' }}>
         {activeTab === 'dom' ? <DomInputs /> : <TableInputs />}
       </div>
     </>
