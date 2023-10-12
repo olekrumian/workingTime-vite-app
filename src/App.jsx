86a764aa7ae06215302d8aad5bf7a993fdc625d7
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-10-12 06:42:23 +0200

    themeChanger

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import Main from './components/Main'
 
 export default function App() {
   useEffect(() => {
-    const hour = new Date().getHours()
-    const body = document.body
+    const setTheme = () => {
+      const hour = new Date().getHours()
+      const body = document.body
 
-    if (hour >= 8 && hour < 19) {
-      body.classList.add('day-theme')
-      body.classList.remove('night-theme')
-    } else {
-      body.classList.add('night-theme')
-      body.classList.remove('day-theme')
+      if (hour >= 8 && hour < 19) {
+        body.classList.add('day-theme')
+        body.classList.remove('night-theme')
+      } else {
+        body.classList.add('night-theme')
+        body.classList.remove('day-theme')
+      }
+    }
+
+    setTheme()
+
+    const intervalId = setInterval(() => {
+      setTheme()
+    }, 60000)
+
+    return () => {
+      clearInterval(intervalId)
     }
   }, [])
 
